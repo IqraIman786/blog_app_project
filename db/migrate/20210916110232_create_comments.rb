59a042962247cf345cf8570acb737918fc31ac14
Iqra Shahzadi <iqra.shahzadi@devsinc.com>
2021-09-16 19:51:49 +0500

    blog_app completed

---
+++ db/migrate/20210916110232_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.timestamps
+      t.references :article, foreign_key: true
+    end
+  end
+end
